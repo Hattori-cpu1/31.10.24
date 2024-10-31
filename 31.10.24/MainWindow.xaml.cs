e4db4f0997325e87ca235e6a7396fdabbe9c4293
FarimetsSA <FarimetsSA@310D-11>
2024-10-31 11:03:34 +0700

    Добавьте файлы проекта.

---
+++ 31.10.24/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _31._10._24
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random _random= new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BMouseEnter(object sender, MouseEventArgs e)
+        {
+            double maxWidth = this.ActualWidth - boba.ActualWidth;
+            double maxHeight = this.ActualHeight - boba.ActualHeight;
+
+            double newX = _random.NextDouble()*maxWidth;
+            double newY = _random.NextDouble() * maxHeight;
+
+            boba.Margin = new Thickness(newX, newY, 0, 0);
+        }
+    }
+}
